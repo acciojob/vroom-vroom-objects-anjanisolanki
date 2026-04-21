b26aff07ee4373c0e5c70f7291b6acab5710fc14
anjanisolanki <anjanisolanki1220@gmail.com>
2026-04-21 17:56:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-// Complete the js code
-function Car(make, model) {}
+// 1. Car Constructor
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Adding method to Car prototype
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+// 2. SportsCar Constructor
+function SportsCar(make, model, topSpeed) {
+  // Use .call() to inherit properties from Car
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// 3. Inheritance Setup
+// Link SportsCar prototype to Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset the constructor property (otherwise it points to Car)
+SportsCar.prototype.constructor = SportsCar;
+
+// Adding method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
